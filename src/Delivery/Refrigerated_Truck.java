6a1b0db8d78a078369c2a28774485af1954fee2f
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-05-09 16:45:54 +1000

    Starter method stubs for truck

--- src/Delivery/Refrigerated_Truck.java
+++ src/Delivery/Refrigerated_Truck.java
@@ -6,4 +6,22 @@ public class Refrigerated_Truck extends Truck {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public int getRemainingCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addTruckItem(Item item, int amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/Delivery/Truck.java
+++ src/Delivery/Truck.java
@@ -2,8 +2,32 @@ package Delivery;
 
 public abstract class Truck {
 
+	private String truckType;
+	private Stock cargo;
+	private int capacity;
+	
 	public Truck() {
-		// TODO Auto-generated constructor stub
+		truckType = null;
+		cargo = null;
+		capacity = null;
 	}
+	
+	public String getTruckType() {
+		return truckType;
+	}
+	
+	public Stock getCargo() {
+		return cargo;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public abstract int getRemainingCapacity();
+	
+	public abstract double getCost();
+	
+	public abstract void addTruckItem(Item item, int amount);
 
 }
