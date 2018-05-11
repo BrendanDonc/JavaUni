0b7baa58256ba7e7cc1af17db68cb5e4115d8092
Mary Millar <mary.millar@connect.qut.edu.au>
2018-05-11 22:36:02 +1000

    Continued work on the Stock 'main' class

--- src/Stock/Stock.java
+++ src/Stock/Stock.java
@@ -1,29 +1,48 @@
 package Stock;
 
-import java.util.ArrayList;
+
+import java.util.HashMap;
 
 
 public class Stock {
 	
-	ArrayList<Item> stockArray;
+	HashMap<Object, Integer> stockArray;
 
 	public Stock() {
-		stockArray = new ArrayList<Item>();
+		stockArray = new HashMap<Object, Integer>();
 		
 	}
 	
 	public void addItem(Item item, int quantity) {
+		//will need to override equals() and hashCode() functions for hashmap w/ custom object to work...
 		
+		System.out.println("Adding "+ quantity + " of " + item + "...");
+		stockArray.put(item, quantity);
+		System.out.println("The new stock size is " + stockArray.size());
 	}
 		
-	public Item[] getItems() {		
-		Item[] stock = new Item[stockArray.size()];
+	public Object[] getItems() {
+		Object[] stock = null;
 		
-		for (int i = 0; i < stockArray.size(); i++) {
-			stock[i] = stockArray.get(i);
+		if (stockArray.size() > 0) {
+			stock = stockArray.keySet().toArray();
+
+			return stock;
+			
 		}
 		
-		return stock;
+		else {
+			return stock;
+		}
+	}
+	
+	public void addStock(Stock stock) {
+		
+		Object[] stockConverted = null;
+		stockConverted = stock.stockArray.keySet().toArray();
+		
+		System.out.println(stockConverted);
+		stockArray.putAll(stock.stockArray);
 	}
 	
 	
