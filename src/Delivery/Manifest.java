6e98b26017297e1b889ca0e8090931e4819aaeee
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-05-13 11:44:14 +1000

    Added manifest methods

--- src/Delivery/Manifest.java
+++ src/Delivery/Manifest.java
@@ -1,9 +1,35 @@
 package Delivery;
 
+import java.util.ArrayList;
+
 public class Manifest {
+	private ArrayList<Truck> manifest;
 
 	public Manifest() {
-		// TODO Auto-generated constructor stub
+		manifest = new ArrayList<Truck>();
+	}
+	
+	public void addTruck(Truck truck) {
+		manifest.add(truck);
+	}
+	
+	public ArrayList<Truck> returnManifest(){
+		return manifest;
+	}
+	
+	public String printManifest() {
+		String print = "";
+		if (manifest.isEmpty()) {
+			return print;
+		}
+		else {
+			
+			for (Truck truck : manifest) {
+				print = print + ">" + truck.getTruckType() + "\n";
+				print = print + truck.getCargo().getManifestPrintStyle();
+			}
+			return print;
+		}
 	}
 	
 
