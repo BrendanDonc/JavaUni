d53e5a288e2041f2488e1ba59d4774cc651258a9
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-05-17 19:17:04 +1000

    Completed LoadManifest and CSVTest with test being successful

--- src/CSV/CSVTest.java
+++ src/CSV/CSVTest.java
@@ -8,14 +8,49 @@ import org.junit.Test;
 
 import Delivery.DeliveryException;
 import Stock.StockException;
+import Stock.Store;
 
 public class CSVTest {
 
 	@Test
 	public void test() throws CSVFormatException, StockException, DeliveryException, IOException {
 		InitializeItems.InitializeItems("item_properties.csv");
+		System.out.println(Store.getInstance().getCapitalString());
 		ExportManifest.ExportManifestCSV("Manifest.csv");
-		//LoadSales.LoadSales("sales_log_0.csv");
+		LoadManifest.LoadManifest("Manifest.csv");
+		System.out.println(Store.getInstance().getCapitalString() + "\n");
+		
+		
+		LoadSales.LoadSales("sales_log_0.csv");
+		System.out.println(Store.getInstance().getCapitalString());
+		ExportManifest.ExportManifestCSV("Manifest.csv");
+		LoadManifest.LoadManifest("Manifest.csv");
+		System.out.println(Store.getInstance().getCapitalString() + "\n");
+		
+		
+		LoadSales.LoadSales("sales_log_1.csv");
+		System.out.println(Store.getInstance().getCapitalString());
+		ExportManifest.ExportManifestCSV("Manifest.csv");
+		LoadManifest.LoadManifest("Manifest.csv");
+		System.out.println(Store.getInstance().getCapitalString() + "\n");
+		
+		LoadSales.LoadSales("sales_log_2.csv");
+		System.out.println(Store.getInstance().getCapitalString());
+		ExportManifest.ExportManifestCSV("Manifest.csv");
+		LoadManifest.LoadManifest("Manifest.csv");
+		System.out.println(Store.getInstance().getCapitalString() + "\n");
+		
+		LoadSales.LoadSales("sales_log_3.csv");
+		System.out.println(Store.getInstance().getCapitalString());
+		ExportManifest.ExportManifestCSV("Manifest.csv");
+		LoadManifest.LoadManifest("Manifest.csv");
+		System.out.println(Store.getInstance().getCapitalString() + "\n");
+		
+		LoadSales.LoadSales("sales_log_4.csv");
+		System.out.println(Store.getInstance().getCapitalString());
+		ExportManifest.ExportManifestCSV("Manifest.csv");
+		LoadManifest.LoadManifest("Manifest.csv");
+		System.out.println(Store.getInstance().getCapitalString());
 	}
 
 }
--- src/CSV/LoadManifest.java
+++ src/CSV/LoadManifest.java
@@ -1,5 +1,122 @@
 package CSV;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import Delivery.*;
+import Stock.*;
+
 public class LoadManifest {
+
+	public static void LoadManifest(String file) throws DeliveryException, CSVFormatException, IOException, StockException  {
+		Manifest manifest;
+		Stock stock;
+		
+		//Create the manifest from the csv
+		try {
+			manifest = ReadManifestCSV(file);
+		} catch (CSVFormatException | StockException | DeliveryException | IOException e1) {
+			throw e1;
+		}
+		
+		//lower the capital by the manifest cost
+		try {
+			Store.getInstance().lowerCapital(manifest.sumManifestCost());
+		} catch (DeliveryException e) {
+			throw e;
+		}
+		
+		//add all the items from the trucks to the store
+		for(Truck truck: manifest.returnManifest()) {
+			stock = truck.getCargo();
+			Store.getInstance().getInventory().addStock(stock);
+		}
+		
+	}
+	
+	
+	
+	public static Manifest ReadManifestCSV(String file) throws DeliveryException, CSVFormatException, IOException, StockException {
+		Manifest importManifest = new Manifest();
+		
+		if(new File(file).isFile()) {
+			
+	        BufferedReader br = null;
+	        String line = "";
+	        String csvSplitBy = ",";
+	        String name;
+	        String truckType;
+	        int quantity;
+	        int truck = -1;
+
+	        try {
+
+	            br = new BufferedReader(new FileReader(file));
+	            while ((line = br.readLine()) != null) {
+	            	name = null;
+	            	truckType = null;
+	            	quantity = -1;
+	            	String[] item = line.split(csvSplitBy);
+	            	if(item.length == 1) {
+	            		
+	            		truckType = item[0];
+	            		try {
+							importManifest.importTruck(truckType);
+						} catch (DeliveryException e) {
+							throw e;
+						}
+	            		truck++;
+	            	}
+	            	else if(item.length == 2) {
+	            		
+	            		if(truck == -1) {
+	            			throw new CSVFormatException("Tried to load items in manifest before loading a truck");
+	            		}
+	            		name = item[0];
+	            		try {
+	                		quantity = Integer.parseInt(item[1]);
+	                	}
+	                	catch (NumberFormatException e){
+	                		throw new CSVFormatException("Sale for item " + name +  " has an invalid amount");
+	                	}
+	            		
+	            		try {
+							importManifest.returnManifest()[truck].getCargo().addItemName(name, quantity);
+						} catch (StockException e) {
+							throw e;
+						}
+	            		
+	            		
+	            		
+	            	}
+	            	else {
+	            		throw new CSVFormatException("Manifest line does not have correct amount of fields(1 or 2)");
+	            	}	
+	            }
+	        
+			} catch (FileNotFoundException e) {
+	            throw new CSVFormatException("File does not exist");
+	        } catch (IOException e) {
+	            throw e;
+	        } finally {
+	            if (br != null) {
+	                try {
+	                    br.close();
+	                } catch (IOException e) {
+	                    throw e;
+	                }
+	            }
+	        }
+		}
+		else {
+			throw new CSVFormatException("File does not exist");
+		}
+		
+		
+		return importManifest;
+	}
 
 }
