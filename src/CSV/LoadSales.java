3c7458e1c4951f1e05819f7cf2df4e7ed8e46f82
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-05-17 17:58:47 +1000

    Completed LoadSales

--- src/CSV/LoadSales.java
+++ src/CSV/LoadSales.java
@@ -1,5 +1,93 @@
 package CSV;
 
-public class LoadSales {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+import Stock.*;
+import Stock.StockException;
+
+public class LoadSales {
+	
+	public static void LoadSales(String file) throws StockException, CSVFormatException, IOException{
+		Stock sale;
+
+		try {
+			sale = ReadSaleCSV(file);
+		} catch (CSVFormatException | StockException | IOException e1) {
+			throw e1;
+		}
+
+		try {
+			if(Store.getInstance().getInventory().validSubtractStock(sale)) {
+				try {
+					Store.getInstance().getInventory().subtractStock(sale);
+				} catch (StockException e) {
+					throw e;
+				}
+				Store.getInstance().raiseCapital(sale.sumPrices());
+			}
+		} catch (StockException e) {
+			throw e;
+		}
+		
+
+	}
+	
+	public static Stock ReadSaleCSV(String file) throws CSVFormatException, StockException, IOException {
+		String csvFile = file;
+		Stock sale = new Stock();
+		
+		if(new File(csvFile).isFile()) {
+			
+	        BufferedReader br = null;
+	        String line = "";
+	        String csvSplitBy = ",";
+	        String name;
+	        int quantity;
+
+	        try {
+
+	            br = new BufferedReader(new FileReader(csvFile));
+	            while ((line = br.readLine()) != null) {
+	            	name = null;
+	            	quantity = -1;
+	            	String[] item = line.split(csvSplitBy);
+	            	if(item.length == 2) {
+	            		name = item[0];
+	            		try {
+	                		quantity = Integer.parseInt(item[1]);
+	                	}
+	                	catch (NumberFormatException e){
+	                		throw new CSVFormatException("Sale for item " + name +  " has an invalid amount");
+	                	}
+	            		sale.addItemName(name, quantity);
+	            	}
+	            	else {
+	            		throw new CSVFormatException("Sale does not have correct amount of fields(2)");
+	            	}	
+	            }
+	        
+			} catch (FileNotFoundException e) {
+	            throw new CSVFormatException("File does not exist");
+	        } catch (IOException e) {
+	            throw e;
+	        } finally {
+	            if (br != null) {
+	                try {
+	                    br.close();
+	                } catch (IOException e) {
+	                    throw e;
+	                }
+	            }
+	        }
+		}
+		else {
+			throw new CSVFormatException("File does not exist");
+		}
+		return sale;
+	}
+	
 }
